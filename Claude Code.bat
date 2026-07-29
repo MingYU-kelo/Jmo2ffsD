a2267db47d59890a1744e50edc88db17cbdeec71
MingYU-kelo <MingYU-kelo@users.noreply.github.com>
2026-07-29 23:46:04 +0800

    Initial commit

---
+++ Claude Code.bat
@@ -0,0 +1,10 @@
+set DISABLE_AUTOUPDATER=1
+set ANTHROPIC_BASE_URL=https://api.deepseek.com/anthropic
+set ANTHROPIC_AUTH_TOKEN=sk-b3a891cecc3d4957968a72de588fc427
+set ANTHROPIC_MODEL=deepseek-v4-pro[1m]
+set ANTHROPIC_DEFAULT_OPUS_MODEL=deepseek-v4-pro[1m]
+set ANTHROPIC_DEFAULT_SONNET_MODEL=deepseek-v4-pro[1m]
+set ANTHROPIC_DEFAULT_HAIKU_MODEL=deepseek-v4-flash
+set CLAUDE_CODE_SUBAGENT_MODEL=deepseek-v4-flash
+set CLAUDE_CODE_EFFORT_LEVEL=max
+Claude
